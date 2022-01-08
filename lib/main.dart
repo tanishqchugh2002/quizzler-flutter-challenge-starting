ed97a26b4aba075260fcbd56fe097fa4d3ede286
Tanishq Chugh <tanishqchugh85@gmail.com>
2022-01-08 21:36:41 +0530

    score corrected

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,10 @@ class _QuizPageState extends State<QuizPage> {
             width: 120,
           )
         ],
-      ).show();}
+      ).show();
+        score=0;
+
+      }
 
       else{
       if (userPickedAnswer == correctAnswer) {
@@ -142,9 +145,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
